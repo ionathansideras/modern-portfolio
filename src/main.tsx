ead54fff17d9a73622df359c6b397cff1ac1c632
Ionathan Sideras <iona8ansideras@gmail.com>
2025-05-25 01:18:24 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./styles/index.scss";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
             <App />
+            <Analytics />
         </Router>
     </StrictMode>
 );
