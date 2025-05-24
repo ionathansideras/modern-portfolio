c6df8c4bf7e3889ae1e0ca0a3c4dbe120f2c9850
Ionathan Sideras <iona8ansideras@gmail.com>
2025-05-25 01:25:57 +0300

    commit

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./styles/index.scss";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <Router>
             <App />
             <Analytics />
+            <SpeedInsights />
         </Router>
     </StrictMode>
 );
